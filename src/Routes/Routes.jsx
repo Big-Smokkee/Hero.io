353083de66e006b9b825b8ba9a3a059c299e60ab
Big-Smokkee <fardinfaraz13579@gmail.com>
2026-04-18 22:34:20 +0600

    Allah valo jane ki hobe

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,14 +1,26 @@
 import React from 'react';
 import { createBrowserRouter } from "react-router";
+import Root from '../pages/Root/Root';
+import Home from '../pages/Home/Home';
 
 export const router = createBrowserRouter([
     {
         path: "/",
-        element: <div>Hello World</div>,
+        Component: Root,
+        children: [
+            {
+                index: true,
+                Component: Home
+            },
+            {
+                path: '/apps',
+                element: <div>ki je kori</div>
+            }
+        ]
     },
     {
-        path: '/about',
-        element: <div>ki je kori</div>
+
     }
 ]);
+
 
