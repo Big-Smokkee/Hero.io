7dc5c6251b54f3c721b2aa5f4136debcbb4295fa
Big-Smokkee <fardinfaraz13579@gmail.com>
2026-04-19 23:23:41 +0600

    feat: add trending App section for the homepage with 4 grid layout

---
+++ src/components/TrendindApps/TrendindApps.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import TrendingApp from '../TrendingApp/TrendingApp';
+
+const TrendindApps = ({ appsData }) => {
+    console.log(appsData)
+    const trendingApp = appsData.slice(0, 8);
+    return (
+        <div className='mt-20 inter'>
+            <h2 className='text-[#001931] text-5xl text-center mb-4'>Trending Apps</h2>
+            <p className='text-[#627382] text-xl text-center mb-10'>Explore All Trending Apps on the Market developed by us</p>
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 px-6 md:px-20'>
+                {
+                    trendingApp.map(app => <TrendingApp app={app} key={app.id}></TrendingApp>)
+                }
+            </div>
+            <div className='flex justify-center mt-10 mb-20'>
+
+                <button className='bg-linear-to-r from-[#632EE3] to-[#9F62F2] text-white px-10 py-4 rounded-xl font-semibold'>
+                    Show All
+                </button>
+            </div>
+
+        </div>
+    );
+};
+
+export default TrendindApps;
