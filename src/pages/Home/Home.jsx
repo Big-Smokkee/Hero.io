16bd671a0f28fee183b063662eea240fbc154568
Big-Smokkee <fardinfaraz13579@gmail.com>
2026-04-19 23:24:53 +0600

    featL add Trending Apps section for the HERO.IO

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import Hero from '../../components/Hero/Hero';
 import Sticker from '../../components/Sticker/Sticker';
+import { useLoaderData } from 'react-router';
+import TrendindApps from '../../components/TrendindApps/TrendindApps';
 
 const Home = () => {
+    const appsData = useLoaderData();
     return (
         <div className='text-black'>
             <Hero></Hero>
             <Sticker></Sticker>
+            <TrendindApps appsData={appsData} key={appsData.id}></TrendindApps>
         </div>
     );
 };
