d3860e90f71ae11ac41c39ad55f664bec5e947ba
Big-Smokkee <fardinfaraz13579@gmail.com>
2026-04-19 23:24:10 +0600

    feat: add app card

---
+++ src/components/TrendingApp/TrendingApp.jsx
@@ -0,0 +1,25 @@
+import { Download, Star } from 'lucide-react';
+import React from 'react';
+
+const TrendingApp = ({ app }) => {
+    console.log(app);
+    const { companyName, title, downloads, ratingAvg, image } = app;
+    return (
+        <div className='w-70 h-full flex flex-col rounded-2xl border border-[#D9D9D9] p-4 shadow'>
+            <img src={image} alt="image alter" className='w-full h-79 rounded-xl mb-4' />
+            <h4 className='text-xl font-medium mb-4'>{companyName}: {title}</h4>
+            <div className='flex justify-between items-center'>
+                <div className='text-[#00D390] font-medium p-1.5 bg-[#F1F5E8] rounded flex gap-2 items-center'>
+                    <span><Download size={16} ></Download></span>
+                    <p>{downloads}</p>
+                </div>
+                <div className='text-[#FF8811] font-medium p-1.5 bg-[#FFF0E1] rounded flex gap-2 items-center'>
+                    <span><Star size={16} /></span>
+                    <p>{ratingAvg}</p>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default TrendingApp;
