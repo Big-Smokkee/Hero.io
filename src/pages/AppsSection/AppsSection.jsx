9518e01fb9bd6d3acec6a14f73900c27a522d39d
Big-Smokkee <fardinfaraz13579@gmail.com>
2026-04-21 22:45:31 +0600

    feat: remove the suseState

--- src/pages/AppsSection/AppsSection.jsx
+++ src/pages/AppsSection/AppsSection.jsx
@@ -1,13 +1,13 @@
-import React, { use, useState } from 'react';
+import React, { use } from 'react';
 import TrendingApp from '../../components/TrendingApp/TrendingApp';
 import './AppsSection.css'
 
 const AppsSection = ({ appDataPromise }) => {
     const appData = use(appDataPromise);
-    const [numberOfApps, setNumberOfApps] = useState(appData.length);
+    // const [numberOfApps, setNumberOfApps] = useState(appData.length);
 
     return (
-        <div className='text-black mt-20 mb-20 px-6 md:px-20 inter'>
+        <div className='text-black mt-20 mb-20 px-6 md:px-20 inter' >
             <div className='mb-10 text-center'>
                 <h2 className='text-[#001931] text-5xl font-bold mb-4'>Our All Applications</h2>
                 <p className='text-[#627382] text-xl'>Explore All Apps on the Market developed by us. We code for Millions</p>
@@ -15,7 +15,7 @@ const AppsSection = ({ appDataPromise }) => {
 
             <div className='flex items-center justify-between mb-6'>
                 <div className='flex gap-1 text-2xl text-[#001931]'>
-                    <span>({numberOfApps})</span>
+                    <span>({appData.length})</span>
                     <p>Apps Found</p>
                 </div>
                 <div>
