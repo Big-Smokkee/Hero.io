34a62d3db1aa880eedb7962e6eb51d60e4fad727
Big-Smokkee <fardinfaraz13579@gmail.com>
2026-04-30 11:26:46 +0600

    feat: add link to app details where any one can tap on the apps for seeing details

--- src/components/InstalledApps/InstalledApps.jsx
+++ src/components/InstalledApps/InstalledApps.jsx
@@ -3,6 +3,7 @@ import { Download, HandPlatter, Star } from 'lucide-react';
 import { deleteFromInstalledApps } from '../../utilities/addToInstalledApps';
 import { toast } from 'react-toastify';
 import './InstalledApps.css';
+import { Link } from 'react-router';
 const InstalledApps = ({ app }) => {
     const { companyName, title, downloads, ratingAvg, image, size, id } = app;
     const handleRemoveFromList = (id) => {
@@ -10,33 +11,35 @@ const InstalledApps = ({ app }) => {
         toast(`${title} is uninstalled successfully`);
     }
     return (
-        <div className='flex items-center justify-between p-4 border border-[#D9D9D9] rounded-2xl my-3 inter'>
-            <div className='flex items-center gap-4'>
-                <img src={image} alt="image" className='w-60 h-auto rounded-xl' />
-                <div>
-                    <div className='flex text-[#001931] font-medium text-xl gap-1'>
-                        <p>{companyName}: </p>
-                        <p>{title}</p>
-                    </div>
-                    <div className='flex items-center gap-4'>
-                        <div className='flex items-center gap-1 font-medium text-[#00D390]'>
-                            <span><Download size={16} ></Download></span>
-                            <p>{downloads}</p>
+        <Link to={`/apps/${id}`} className='cursor-pointer'>
+            <div className='flex items-center justify-between p-4 border border-[#D9D9D9] rounded-2xl my-3 inter'>
+                <div className='flex items-center gap-4'>
+                    <img src={image} alt="image" className='w-60 h-auto rounded-xl' />
+                    <div>
+                        <div className='flex text-[#001931] font-medium text-xl gap-1'>
+                            <p>{companyName}: </p>
+                            <p>{title}</p>
                         </div>
-                        <div className='flex items-center gap-1 text-[#FF8811]'>
-                            <span><Star size={16} /></span>
-                            <p>{ratingAvg}</p>
-                        </div>
-                        <div className='flex items-center text-[#627382]'>
-                            <p>{size} MB</p>
+                        <div className='flex items-center gap-4'>
+                            <div className='flex items-center gap-1 font-medium text-[#00D390]'>
+                                <span><Download size={16} ></Download></span>
+                                <p>{downloads}</p>
+                            </div>
+                            <div className='flex items-center gap-1 text-[#FF8811]'>
+                                <span><Star size={16} /></span>
+                                <p>{ratingAvg}</p>
+                            </div>
+                            <div className='flex items-center text-[#627382]'>
+                                <p>{size} MB</p>
+                            </div>
                         </div>
                     </div>
                 </div>
+                <div>
+                    <button className='px-4 py-3 bg-[#00D390] text-white rounded font-semibold cursor-pointer' onClick={() => handleRemoveFromList(id)}>Uninstall</button>
+                </div>
             </div>
-            <div>
-                <button className='px-4 py-3 bg-[#00D390] text-white rounded font-semibold cursor-pointer' onClick={() => handleRemoveFromList(id)}>Uninstall</button>
-            </div>
-        </div>
+        </Link>
     );
 };
 
