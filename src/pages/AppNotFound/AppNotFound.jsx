04c6c265f43e521e29466dab28a037d0e2047a82
Big-Smokkee <fardinfaraz13579@gmail.com>
2026-04-30 11:23:16 +0600

    feat: changed the not found png if an app cant be found

--- src/pages/AppNotFound/AppNotFound.jsx
+++ src/pages/AppNotFound/AppNotFound.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import img from '../../assets/error-404.png';
+import notFoundPng from '../../assets/App-Error.png';
 import { useNavigate } from 'react-router';
 const AppNotFound = () => {
     const navigate = useNavigate();
     return (
-        <div className='flex flex-col items-center justify-center gap-4 text-black my-10'>
-            <img src={img} alt="error image" />
+        <div className='flex flex-col items-center justify-center gap-4 text-black my-10 inter'>
+            <img src={notFoundPng} alt="error image" />
             <h3 className='text-violet-950 text-5xl'>App Is Not Found</h3>
             <p className='text-lg text-gray-500'>The app you are looking for does not exist or has been removed</p>
             <button className='px-4 py-3 rounded-xl font-semibold bg-violet-950 text-white cursor-pointer' onClick={() => navigate('/')}>Back To Home</button>
