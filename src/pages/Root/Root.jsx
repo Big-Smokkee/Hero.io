84e61ba7484794ee2372c8de0e14ef5a69228e79
Big-Smokkee <fardinfaraz13579@gmail.com>
2026-04-30 11:28:09 +0600

    add font universelly

--- src/pages/Root/Root.jsx
+++ src/pages/Root/Root.jsx
@@ -3,10 +3,11 @@ import Navbar from '../../components/Navbar/Navbar';
 import { Outlet } from 'react-router';
 import Footer from '../../components/Footer/Footer';
 import { ToastContainer } from 'react-toastify';
+import './Root.css';
 
 const Root = () => {
     return (
-        <div className='bg-white max-h-full'>
+        <div className='bg-white max-h-full inter'>
             <Navbar></Navbar>
             <Outlet></Outlet>
             <Footer></Footer>
