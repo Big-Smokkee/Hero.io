1a2bf8002fd79a0287a4dfc6668b1f9f9f5e9dda
Big-Smokkee <fardinfaraz13579@gmail.com>
2026-04-30 13:07:07 +0600

    feat: updated code because of routing loading

--- src/pages/Root/Root.jsx
+++ src/pages/Root/Root.jsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import Navbar from '../../components/Navbar/Navbar';
-import { Outlet } from 'react-router';
+import { Outlet, useNavigation } from 'react-router';
 import Footer from '../../components/Footer/Footer';
 import { ToastContainer } from 'react-toastify';
 import './Root.css';
+import LoadingScreen from '../LoadingScreen/LoadingScreen';
 
 const Root = () => {
+    const navigation = useNavigation();
+    console.log(navigation);
+    console.log(navigation.state);
     return (
         <div className='bg-white max-h-full inter'>
             <Navbar></Navbar>
+            {navigation.state === "loading" && <LoadingScreen></LoadingScreen>}
             <Outlet></Outlet>
             <Footer></Footer>
             <ToastContainer></ToastContainer>
