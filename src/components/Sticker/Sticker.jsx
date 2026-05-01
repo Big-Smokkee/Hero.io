4f622b705acb896c0aa0e4542db32ba5e7d6228a
Big-Smokkee <fardinfaraz13579@gmail.com>
2026-05-01 11:27:32 +0600

    fix the responsiveness

--- src/components/Sticker/Sticker.jsx
+++ src/components/Sticker/Sticker.jsx
@@ -3,8 +3,8 @@ import './Sticker.css'
 const Sticker = () => {
     return (
         <div className="w-full bg-linear-to-br from-[#632EE3] to-[#9F62F2] text-white py-12 px-6  flex flex-col items-center justify-center inter">
-            <h2 className='text-5xl mb-10 font-semibold'>Trusted by Millions, Built for You</h2>
-            <div className='flex items-center justify-between gap-16'>
+            <h2 className='text-5xl mb-10 font-semibold text-center'>Trusted by Millions, Built for You</h2>
+            <div className='flex flex-col md:flex-row items-center justify-between gap-4 md:gap-16'>
                 <div className='flex flex-col items-center justify-center gap-4'>
                     <p className='text-gray-300'>Total Downloads</p>
                     <h3 className='text-6xl font-bold'>29.6M</h3>
