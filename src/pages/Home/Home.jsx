8a01b4fe97b5cc58fcf2810357ee24ddc62b7f8a
Big-Smokkee <fardinfaraz13579@gmail.com>
2026-05-01 12:46:33 +0600

    feat: removed app promises and add loader

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,13 @@
-import React, { use } from 'react';
+import React from 'react';
 import Hero from '../../components/Hero/Hero';
 import Sticker from '../../components/Sticker/Sticker';
 import TrendindApps from '../../components/TrendindApps/TrendindApps';
+import { useLoaderData } from 'react-router';
 
-const Home = ({ appDataPromise }) => {
-    const appsData = use(appDataPromise);
+const Home = () => {
+    // const Home = ({ appDataPromise }) => {
+    // const appsData = use(appDataPromise);
+    const appsData = useLoaderData();
     return (
         <div className='text-black'>
             <Hero></Hero>
