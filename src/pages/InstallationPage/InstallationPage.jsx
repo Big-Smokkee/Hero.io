31a6ec470961e9ff0856f8220d648cc6d4c09313
Big-Smokkee <fardinfaraz13579@gmail.com>
2026-05-01 12:00:03 +0600

    fix responsiveness

--- src/pages/InstallationPage/InstallationPage.jsx
+++ src/pages/InstallationPage/InstallationPage.jsx
@@ -50,29 +50,29 @@ const InstallationPage = () => {
 
 
     return (
-        <div className="p-20 text-black inter">
+        <div className="p-6 md:p-20 text-black inter">
             <div className='text-center'>
-                <h2 className="text-5xl font-semibold mb-4 text-[#001931]">Your Installed Apps</h2>
+                <h2 className="text-4xl md:text-5xl font-semibold mb-6 text-[#001931]">Your Installed Apps</h2>
                 <p className='text-[#627382]'>Explore All Trending Apps on the Market developed by us</p>
             </div>
-            <div className='flex items-center justify-between mb-6'>
-                <div className='flex gap-1 text-2xl text-[#001931]'>
+            <div className='flex items-center justify-between mb-6 gap-2'>
+                <div className='flex gap-1 text-xl md:text-2xl text-[#001931] w-1/2'>
                     <span>{myInstalledApps.length}</span>
                     <p>Apps Found</p>
                 </div>
-                <div>
-                    <div className="dropdown dropdown-end">
-                        <div tabIndex={0} role="button" className="btn m-1 flex items-center gap-1 bg-white text-black border-[#D9D9D9]">
-                            <p>{sortedApps}</p>
-                            {/* <ChevronDown /> */}
-                            <ChevronDown></ChevronDown>
-                        </div>
-                        <ul tabIndex="-1" className="dropdown-content menu bg-white rounded-box z-1 w-52 p-2 shadow-sm border-[#D9D9D9] border">
-                            <li><a onClick={() => handleSortedApps("highToLow")}>High - Low</a></li>
-                            <li><a onClick={() => handleSortedApps("lowToHigh")}>Low - High</a></li>
-                        </ul>
+
+                <div className="dropdown dropdown-end w-1/2">
+                    <div tabIndex={0} role="button" className="btn m-1 flex flex-col md:flex-row items-center gap-1 bg-white text-black border-[#D9D9D9]">
+                        <p>{sortedApps}</p>
+                        {/* <ChevronDown /> */}
+                        <ChevronDown></ChevronDown>
                     </div>
+                    <ul tabIndex="-1" className="dropdown-content menu bg-white rounded-box z-1 w-52 p-2 shadow-sm border-[#D9D9D9] border">
+                        <li><a onClick={() => handleSortedApps("highToLow")}>High - Low</a></li>
+                        <li><a onClick={() => handleSortedApps("lowToHigh")}>Low - High</a></li>
+                    </ul>
                 </div>
+
             </div>
             <div>
                 {
