142fa606d4dbca85154d9f74729a4ee93533d213
Big-Smokkee <fardinfaraz13579@gmail.com>
2026-05-01 12:26:02 +0600

    fix responsiveness

--- src/pages/AppNotFound/AppNotFound.jsx
+++ src/pages/AppNotFound/AppNotFound.jsx
@@ -4,10 +4,10 @@ import { useNavigate } from 'react-router';
 const AppNotFound = () => {
     const navigate = useNavigate();
     return (
-        <div className='flex flex-col items-center justify-center gap-4 text-black my-10 inter'>
+        <div className='flex flex-col items-center justify-center gap-4 text-black my-10 inter p-6 md:p-20'>
             <img src={notFoundPng} alt="error image" />
-            <h3 className='text-violet-950 text-5xl'>App Is Not Found</h3>
-            <p className='text-lg text-gray-500'>The app you are looking for does not exist or has been removed</p>
+            <h3 className='text-violet-950 text-4xl md:text-5xl text-center'>App Is Not Found</h3>
+            <p className='text-lg text-gray-500 text-center'>The app you are looking for does not exist or has been removed</p>
             <div className='flex gap-4'>
                 <button className='px-4 py-3 rounded-xl font-semibold bg-violet-950 text-white cursor-pointer' onClick={() => navigate('/')}>Back To Home</button>
                 <button className='px-4 py-3 rounded-xl font-semibold bg-violet-950 text-white cursor-pointer' onClick={() => navigate(-1)}>Go Back</button>
